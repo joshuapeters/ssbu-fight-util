1af1d0ccde25ebe0426560b803df207e131765e4
JP <jgpeters717@gmail.com>
2019-06-20 07:33:36 -0400

    stubbed out a character read conductor

---
+++ src/01_core/conductors/characters/CharacterReadConductor.ts
@@ -0,0 +1,21 @@
+const findAll = () => {
+    return [
+        {
+            "test": "test!!!"
+        },
+        {
+            "test2": "another test!!!"
+        }
+    ];
+}
+
+const findById = (id: number | string) => {
+    return {
+        id : "test!!!"
+    };
+}
+
+export {
+    findAll,
+    findById
+}
